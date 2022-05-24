f722f3b62c497b57da03d0d382ab19f6268acbde
Joydip <joydip.mondal@gmail.com>
2022-05-24 16:20:28 +0530

    day-1 post lunch update

--- codes/CSharp_OOP/CSharp_OOP/Program.cs
+++ codes/CSharp_OOP/CSharp_OOP/Program.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using EmployeeLibrary;
+﻿using EmployeeLibrary;
+using System;
 
 namespace CSharp_OOP
 {
@@ -11,8 +7,22 @@ namespace CSharp_OOP
     {
         static void Main()
         {
-            Employee employeeObj = new Employee();
+            //Employee employeeObj = new Employee("Joydip", 1, 1000, 2000, 3000);
+            //C# 4.0 (2010) -- named arguments
+            Employee joydipEmployeeObj = new Employee(id: 1, basicPay: 1000, name: "joydip", daPay: 2000, hraPay: 3000);
+
+            Employee anilEmployeeObj = new Employee(id: 2, basicPay: 2000, name: "Anil", daPay: 3000, hraPay: 4000);
+
+            //joydipEmployeeObj.SetProjectName("CITA");
+            //anilEmployeeObj.SetProjectName("SSIS");
+
+            joydipEmployeeObj.ProjectName = "CITA";
+            anilEmployeeObj.ProjectName = "SSIS";
+
+            Console.WriteLine(joydipEmployeeObj.ProjectName);
+
             int x = 10;
+            Console.WriteLine(x);
         }
     }
 }
--- codes/DOTNET.BOSCH.EMS/DOTNET.BOSCH.EMS.UserInterface/Program.cs
+++ codes/DOTNET.BOSCH.EMS/DOTNET.BOSCH.EMS.UserInterface/Program.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DOTNET.BOSCH.EMS.BusinessEntities;
 
 namespace DOTNET.BOSCH.EMS.UserInterface
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            //Employee employee = new Employee("joydip", 1, 1000, 2000, 3000);
         }
     }
 }
