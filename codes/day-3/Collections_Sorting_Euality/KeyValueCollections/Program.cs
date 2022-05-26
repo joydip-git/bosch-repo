b365f063690aa40a9f14dd7c9954845e884226da
Joydip <joydip.mondal@gmail.com>
2022-05-26 11:47:56 +0530

    task uploaded

---
+++ codes/day-3/Collections_Sorting_Euality/KeyValueCollections/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyValueCollections
+{
+    class A : IComparable<A>
+    {
+        int id;
+        char keyName;
+
+        public A(int id, char key)
+        {
+            this.id = id;
+            this.keyName = key;
+        }
+
+        public int CompareTo(A other)
+        {
+            return this.id.CompareTo(other.id);
+        }
+        public override string ToString()
+        {
+            return this.keyName.ToString();
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            /*
+            Hashtable hashtable = new Hashtable();
+            hashtable.Add(1, "bosch");
+            hashtable.Add('a', 12);
+            hashtable.Add("str", 'R');
+            //hashtable.Remove('a');
+            hashtable['a'] = 98.45;
+            //if the key does not exist in the collection, then the key and the value will be added
+            hashtable[2] = 67;
+            foreach (DictionaryEntry item in hashtable)
+            {
+                Console.WriteLine(item.Key + ":" + item.Value);
+            }
+
+            Console.WriteLine("\n\n");
+
+            //keys will be sorted
+            //the keys must be of similar type
+            SortedList sortedList = new SortedList();
+            sortedList.Add(1, "bosch");
+            sortedList.Add(0, 12);
+            sortedList.Add(2, 'R');
+            //sortedList.Remove('a');
+            sortedList[2] = 98.45;
+            //if the key does not exist in the collection, then the key and the value will be added
+            sortedList[6] = 67;
+            foreach (DictionaryEntry item in sortedList)
+            {
+                Console.WriteLine(item.Key + ":" + item.Value);
+            }
+
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            dictionary.Add(1, "bosch");
+            dictionary.Add(0, "abcd");
+            dictionary.Add(2, "xyz");
+            dictionary[0] = "some value";
+            foreach (KeyValuePair<int, string> item in sortedList)
+            {
+                Console.WriteLine(item.Key + ":" + item.Value);
+            }
+
+
+            SortedList<int, string> sortedGenericList = new SortedList<int, string>();
+            sortedGenericList.Add(1, "bosch");
+            sortedGenericList.Add(0, "12");
+            sortedGenericList.Add(2, "R");
+            //sortedList.Remove('a');
+            sortedGenericList[2] = "98.45";
+            //if the key does not exist in the collection, then the key and the value will be added
+            sortedGenericList[6] = "abcd";
+            foreach (KeyValuePair<int, string> item in sortedGenericList)
+            {
+                Console.WriteLine(item.Key + ":" + item.Value);
+            }
+            */
+            SortedList<A, int> d = new SortedList<A, int>();
+            d.Add(new A(2, '2'), 12);
+            d.Add(new A(0, '0'), 15);
+            d.Add(new A(1, '1'), 13);
+
+            foreach (KeyValuePair<A, int> item in d)
+            {
+                Console.WriteLine(item.Key + ":" + item.Value);
+            }
+        }
+    }
+}
---
+++ codes/day-3/Collections_Sorting_Euality/StringType/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringType
+{
+    class Program
+    {
+        //Param array
+        //can be used in method as argument only, to accept N number of arguemnts after any fixed number of argument(s)
+        //there can be only ine declaration with params
+        //that should be the last argument to accept all remaining parameters
+        static string[] Split(string sentence, params char[] separators)
+        {
+            return null;
+        }
+        static void Main()
+        {
+            //params int[] x = new int[4];
+            //"I am a freelance consultant, and currently working for Bosch. I am from Bangalore"
+            /*
+             * word1: count-value
+             * word2: count-value
+             * ..........................
+             * a: 1
+             * am: 2
+             * and: 1
+             * Bangalore:1
+             * Bosch: 1
+             * consultant: 1
+             * currently: 1
+             * .........................
+             * 
+             * 
+             */
+            Console.Write("enter a sentence: ");
+            string value = Console.ReadLine();
+            Split(sentence: value, ' ', ',', '.');
+        }
+    }
+}
