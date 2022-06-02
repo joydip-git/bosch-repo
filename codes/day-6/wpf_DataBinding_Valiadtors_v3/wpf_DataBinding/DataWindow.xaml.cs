2223ee368d9d42148db853c0ef464b3017facad1
Joydip <joydip.mondal@gmail.com>
2022-06-02 15:53:42 +0530

    day-6 data binding with data conversion uodated

---
+++ codes/day-6/wpf_DataBinding_Valiadtors_v3/wpf_DataBinding/DataWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using wpf_DataBinding.Converters;
+
+namespace wpf_DataBinding
+{
+    /// <summary>
+    /// Interaction logic for DataWindow.xaml
+    /// </summary>
+    public partial class DataWindow : Window
+    {
+        private ObservableCollection<Person> people;
+        public DataWindow()
+        {
+            InitializeComponent();
+            //this.Resources.Add("salConveter", new SalaryConverter());
+            //Binding b = new Binding();
+            //b.Converter = this.Resources["salConverter"] as SalaryConverter;
+            //b.Converter = new SalaryConverter();
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            people = PersonRepository.People;
+            comboPeople.ItemsSource = people;
+            comboPeople.DisplayMemberPath = "Name";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in people)
+            {
+                MessageBox.Show(item.Name);
+            }
+        }
+
+        private void btnIncrease_Click(object sender, RoutedEventArgs e)
+        {           
+            try
+            {
+                foreach (var p in people)
+                {
+                    p.Salary *= 2;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
---
+++ codes/day-6/wpf_DataBinding_v1/wpf_DataBinding/DataWindowPersonViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_DataBinding
+{
+    public class DataWindowPersonViewModel : INotifyPropertyChanged
+    {
+        private Person person;
+
+        public Person Person
+        {
+            get { return person; }
+            set
+            {
+                person = value;
+                OnPropertyChanged("Person");
+            }
+        }
+
+        public string ConvertedSalary
+        {
+            set
+            {
+                double result;
+                if (double.TryParse(value, NumberStyles.Any, null, out result))
+                {
+                    person.Salary = result;
+                }
+                else
+                {
+                    throw new ArgumentException("error");
+                }
+                OnPropertyChanged("ConvertedSalary");
+            }
+            get => person.Salary.ToString("C", new CultureInfo("en-US"));
+        }
+
+        public string BackColor
+        {
+            get => person.Salary > 10000 ? "Grey" : "Wheat";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
