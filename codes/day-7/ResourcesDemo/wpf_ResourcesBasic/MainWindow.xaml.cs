f55106b9abeb09a8a9e59fcfc51d9b970cb21dba
Joydip <joydip.mondal@gmail.com>
2022-06-03 11:29:05 +0530

    day-7 Resources basics

---
+++ codes/day-7/ResourcesDemo/wpf_ResourcesBasic/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf_ResourcesBasic
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //ImageBrush imageBrush = new ImageBrush();
+            //imageBrush.Viewport = new Rect(0, 0, 32, 32);
+            //imageBrush.ViewportUnits = BrushMappingMode.Absolute;
+            //imageBrush.ImageSource = "";
+            //imageBrush.Opacity = 0.3;
+            //this.Resources.Add("k1", new ImageBrush());
+        }
+    }
+}
