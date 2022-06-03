8893ac87a04a17cc7d77040e846d8138f1c0f8cd
Joydip <joydip.mondal@gmail.com>
2022-06-03 16:06:34 +0530

    partial update on custom control

---
+++ codes/day-7/wpf_RoutedEvents/BoschUserControls/ColorEditor.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BoschUserControls
+{
+    /// <summary>
+    /// Interaction logic for ColorEditor.xaml
+    /// </summary>
+    public partial class ColorEditor : UserControl
+    {
+        public static readonly DependencyProperty RedProperty;
+        public static readonly DependencyProperty GreenProperty;
+        public static readonly DependencyProperty BlueProperty;
+        public static readonly DependencyProperty ColorProperty;
+
+        public static readonly RoutedEvent ColorChangedEvent;
+
+        static ColorEditor()
+        {
+            //RedProperty
+            FrameworkPropertyMetadata redPropertyMetadata = new FrameworkPropertyMetadata(new PropertyChangedCallback(OnColorRGBChangedCallback));
+            RedProperty = DependencyProperty.Register("Red", typeof(Color), typeof(ColorEditor), redPropertyMetadata);
+
+            //GreenProperty
+            FrameworkPropertyMetadata greenPropertyMetadata = new FrameworkPropertyMetadata(new PropertyChangedCallback(OnColorRGBChangedCallback));
+            GreenProperty = DependencyProperty.Register("Green", typeof(Color), typeof(ColorEditor), greenPropertyMetadata);
+
+            //BlueProperty
+            FrameworkPropertyMetadata bluePropertyMetadata = new FrameworkPropertyMetadata(new PropertyChangedCallback(OnColorRGBChangedCallback));
+            BlueProperty = DependencyProperty.Register("Blue", typeof(Color), typeof(ColorEditor), bluePropertyMetadata);
+
+            //ColorProperty
+            FrameworkPropertyMetadata colorPropertyMetadata = new FrameworkPropertyMetadata(Colors.Black, new PropertyChangedCallback(OnColorChangedCallback));
+
+            //ColorChangedEvent
+        }
+        public ColorEditor()
+        {
+            InitializeComponent();
+        }
+
+        private static void OnColorRGBChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ColorEditor cd = d as ColorEditor;
+            Color color = cd.Color;
+
+            if (e.Property == RedProperty)
+            {
+                color.R = (byte)e.NewValue;
+            }
+            else if (e.Property == GreenProperty)
+            {
+                color.G = (byte)e.NewValue;
+            }
+            else if (e.Property == BlueProperty)
+            {
+                color.B = (byte)e.NewValue;
+            }
+
+            cd.Color = color;
+        }
+        private static void OnColorChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+
+        public Color Red
+        {
+            get => (Color)this.GetValue(RedProperty);
+            set => this.SetValue(RedProperty, value);
+        }
+        public Color Green
+        {
+            get => (Color)this.GetValue(GreenProperty);
+            set => this.SetValue(GreenProperty, value);
+        }
+        public Color Blue
+        {
+            get => (Color)this.GetValue(BlueProperty);
+            set => this.SetValue(BlueProperty, value);
+        }
+        public Color Color
+        {
+            get => (Color)this.GetValue(ColorProperty);
+            set => this.SetValue(ColorProperty, value);
+        }
+
+        public event EventHandler ColorChanged;
+    }
+}
---
+++ codes/day-7/wpf_RoutedEvents/wpf_RoutedEvents/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf_RoutedEvents
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //this.Initialized <-- NOT a routed event
+            
+            
+        }
+
+        private void mainGrid_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show((e.OriginalSource as Button).Name);
+            MessageBox.Show("handled by grid");
+        }
+
+        private void btnClick_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show((e.OriginalSource as Button).Name);
+            MessageBox.Show("handled by outer button");            
+        }
+
+        private void btnInner_Click(object sender, RoutedEventArgs e)
+        {            
+            MessageBox.Show((e.OriginalSource as Button).Name);
+            MessageBox.Show("handled by inner button");
+            e.Handled = true;
+        }
+    }
+}
