b6d8352586dce2c173df39d25ec43738eebdcf69
Joydip <joydip.mondal@gmail.com>
2022-06-06 11:25:21 +0530

    day-8 styles update-1
    event and property setter done

---
+++ codes/day-8/wpf_Styles/wpf_Styles/ButtonStyleConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_Styles
+{
+    class ButtonStyleConstants
+    {
+        //property setter
+        public double Size { get; private set; } = 20;
+        public string Family => "Aril";
+    }
+}
---
+++ codes/day-8/wpf_Styles/wpf_Styles_EventSetter/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf_Styles_EventSetter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtBlock_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (e.OriginalSource is TextBlock)
+            {
+                (sender as TextBlock).Background = new SolidColorBrush(Colors.Beige);
+            }
+        }
+
+        private void txtBlock_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (e.OriginalSource is TextBlock)
+            {
+                (sender as TextBlock).Background = null;
+            }
+        }
+    }
+}
