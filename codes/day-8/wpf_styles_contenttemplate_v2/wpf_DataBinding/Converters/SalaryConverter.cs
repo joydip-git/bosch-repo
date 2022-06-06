d2bcd8906bc817bfb88ab161ff10a264cef4b2a6
Joydip <joydip.mondal@gmail.com>
2022-06-06 14:42:07 +0530

    apps created

---
+++ codes/day-8/wpf_styles_contenttemplate_v2/wpf_DataBinding/Converters/SalaryConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace wpf_DataBinding.Converters
+{
+    public class SalaryConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double salary = (double)value;
+            return salary.ToString("C", new CultureInfo("en-US"));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string input = (string)value;
+            double salary;
+            return double.TryParse(input, NumberStyles.Any, null, out salary) ? salary : value;
+        }
+    }
+}
